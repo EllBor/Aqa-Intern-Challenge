2e999af01c8ba3d335451639e575d4c0c6e70f48
EllBor <elbor2@mail.ru>
2025-04-12 17:01:21 +0700

    Исправлен тест, написан дополнительный 1 тест

---
+++ tests/widget.page.ts
@@ -0,0 +1,50 @@
+import {Page} from "@playwright/test";
+
+enum WidgetPageSelectors {
+    WRAPPER = '.sc-dino-typography-h > [class^=widget__]',
+    WIDGET_BODY = '[class^=widgetWrapper] > [class^=widget__]',
+    HEADER_TEXT = 'header h5',
+    BUTTON_OPEN = '[data-test=openWidget]',
+    BUTTON_WRITE_TO_US = '.btnText__fCHf9',
+    ARTICLE_POPULAR_TITLE = '[class^=popularTitle__]',
+    ARTICLE_POPULAR_LIST = `${ARTICLE_POPULAR_TITLE} + ul[class^=articles__]`,
+    ARTICLE_POPULAR_LIST_ITEM = `${ARTICLE_POPULAR_LIST} > li`,
+    LOGIN = '#login',
+}
+
+export class WidgetPage {
+    static selector = WidgetPageSelectors;
+
+    constructor(protected page: Page) {}
+
+    wrapper() {
+        return this.page.locator(WidgetPage.selector.WRAPPER)
+    }
+
+    async openWidget() {
+        return this.wrapper().locator(WidgetPage.selector.BUTTON_OPEN).click();
+    }
+
+    async getPopularArticles() {
+        const locator = this.wrapper().locator(WidgetPage.selector.ARTICLE_POPULAR_LIST_ITEM);
+        await locator.first().waitFor({ timeout: 3000 });
+        return locator.all();
+    }
+
+    async clickWriteToUs() {
+        return this.wrapper().locator(WidgetPage.selector.BUTTON_WRITE_TO_US).click();
+    }
+
+    async getTitle() {
+        return this.wrapper().locator(WidgetPage.selector.HEADER_TEXT).textContent();
+    }
+
+    getWidgetBody() {
+        return this.page.locator(WidgetPage.selector.WIDGET_BODY);
+    }
+
+    getLoginInput() {
+        return this.page.locator(WidgetPage.selector.LOGIN);
+    }
+}
+
